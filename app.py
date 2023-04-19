86b9287fb91cb7394434ea63afc81255fd1aa2b3
Richard <richard.m.sullivan2@gmail.com>
2023-04-19 18:27:23 -0500

    created html, css, and routes for login and account creation. This allows the pages to load, but  does not do any real authentification and account creation yet.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv, find_dotenv
 import flask
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 import os
@@ -12,7 +12,6 @@ app = Flask(__name__)
 
 # load environment variables and set app config settings
 load_dotenv(find_dotenv())
-
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config['SECRET_KEY'] = getenv("SECRET_KEY")
 
@@ -20,7 +19,7 @@ app.config['SECRET_KEY'] = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 
 
-# this will serve as the base for the recipe database
+# These classes form the relations in the database 
 
 class Comment(db.Model): #a model for a row, defines a table
     id = db.Column(db.Integer, primary_key=True)
@@ -34,7 +33,7 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(420), nullable=False)
 
-# make sure sql alchemy tables are created
+# make sure database tables are created
 with app.app_context():
     db.create_all()
 
@@ -48,23 +47,55 @@ login_manager.init_app(app)
 def load_user(user_id):
     return User.query.get(user_id)
 
-
-# the following is the base for the login screen
-
-@app.route('/') #this initializes the movie
+# Landing Page: If the user is logged in, go directly to the
+# Dashboard, and if not go to the Login Page
+@app.route('/')
 def start():
-    return flask.render_template("login.html")
+    return redirect(url_for('login'))
 
-@app.route('/login', methods=["POST"])
+# Login Page: from here they can login and be redirected to the Dashboard,
+# or they can go to the create_account page.
+@app.route('/login', methods=['GET','POST'])
 def login():
-    form_data = flask.request.form
+    error = None
+    # if they submitted the login form, check the username and password
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # if the username and password are incorrect, then mark error as true
+        # and send them back to the login page
+        if username != 'admin' or password != 'secret':
+            error = 'Invalid credentials. Please try again.'
+        # otherwise, send to the dashboard
+        else:
+            return redirect(url_for('index'))
+        
+    return render_template('login.html', error=error)
+
+@app.route('/create_account',methods=['GET','POST'])
+def create_account():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if username or email already exist
+        # If so, render the template with an error message
+        # Otherwise, create the new account and redirect to a success page
+        # (or log the user in automatically and redirect to the homepage)
+
+    return render_template('create_account.html')
+
+@app.route('/authenticate', methods=["POST"])
+def autheticate():
+    form_data = request.form
     user = form_data["user"]
     unique = db.session.query(User).filter_by(username=user).first() is None
     if(unique):
         u = User(username=user)
         db.session.add(u)
         db.session.commit()
-    return flask.redirect(flask.url_for("index", user=user))
+    return redirect(url_for("index", user=user))
     
     
 # the following is the base for the recipe list screen
@@ -81,12 +112,12 @@ def index():
 def search():
     #search should be passed via redirect
     #this is to keep the same search while navigating pages
-    search = flask.request.args['search']
+    search = request.args['search']
     
     #some code to break the search into blocks of 10? 20?
     #id refers to the num of blocks to incriment forward by when loading the page
     
-    return flask.render_template("search.html", search=search, id=id)
+    return render_template("search.html", search=search, id=id)
     
     #these should be called by nav buttons on the search page:
     #for prev page
@@ -98,13 +129,13 @@ def search():
 #and also a template for managing button interactions on the page
 @app.route('/submit', methods=["POST"])
 def submit():
-    form_data = flask.request.form
+    form_data = request.form
     c = Comment(movie = form_data["movie"], 
                       username = form_data["user"],
                       rating = form_data["rating"],
                       comment = form_data["comment"])  
     db.session.add(c)
     db.session.commit()
-    return flask.redirect(flask.url_for("index", user=form_data["user"]))
+    return redirect(flask.url_for("index", user=form_data["user"]))
 
 #app.run()
