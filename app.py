654ababbdbe05e0797325c825fb1d8fed153f32e
Richard <richard.m.sullivan2@gmail.com>
2023-04-20 23:28:00 -0500

    implemented password hashing and removed the authentification route because it was never used.

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from dotenv import load_dotenv, find_dotenv
 import flask
+import os
+import requests
+
+from dotenv import load_dotenv, find_dotenv
 from flask import Flask, request, render_template, redirect, url_for
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
-import os
 from os import getenv
-import requests
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 # create the app
 app = Flask(__name__)
@@ -20,7 +23,6 @@ db = SQLAlchemy(app)
 
 
 # These classes form the relations in the database 
-
 class Comment(db.Model): #a model for a row, defines a table
     id = db.Column(db.Integer, primary_key=True)
     movie = db.Column(db.Integer, unique=False, nullable=False)
@@ -32,6 +34,7 @@ class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(420), nullable=False)
+
 
 # make sure database tables are created
 with app.app_context():
@@ -47,6 +50,7 @@ login_manager.init_app(app)
 def load_user(user_id):
     return User.query.get(user_id)
 
+
 # Landing Page: If the user is logged in, go directly to the
 # Dashboard, and if not go to the Login Page
 @app.route('/')
@@ -55,6 +59,7 @@ def start():
         return redirect(url_for('index'))
     else:
         return redirect(url_for('login'))
+
 
 # Login Page: from here they can login and be redirected to the Dashboard,
 # or they can go to the create_account page.
@@ -66,20 +71,22 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        # !!! will need to decrypt password when encription is added!!!
-        user = User.query.filter_by(username=username,password=password).first()
+        # get a user from the database
+        user = User.query.filter_by(username=username).first()
 
-        # if the username and password are incorrect, then mark error as true
-        # and send them back to the login page
-        if user == None:
-            error = 'Invalid credentials. Please try again.'
-        # otherwise, send to the dashboard
-        else:
+        # if the user exists and the password matches, log them in
+        if user and check_password_hash(user.password, password):
             login_user(user)
             return redirect(url_for('start'))
+        # otherwise log an error message
+        else:
+            error = 'Invalid credentials. Please try again.'
         
     return render_template('login.html', error=error)
 
+
+# Create Account: you can arrive here from the login page
+# on this page you create a new acocunt to be stored in the database
 @app.route('/create_account',methods=['GET','POST'])
 def create_account():
     error = None
@@ -87,34 +94,24 @@ def create_account():
         username = request.form['username']
         password = request.form['password']
 
-        # Check if username or email already exist
-        # If so, render the template with an error message
+        # get user from the database
         user = User.query.filter_by(username=username).first()
-        if user != None:
+
+        # if you were able to find a user, then log error
+        if user:
             error = 'Account already exists. Try again.'
-        # Otherwise, create the new account and redirect to a success page
-        # (or log the user in automatically and redirect to the homepage)
+        # otherwise go back to the login page
         else:
-            # !!!this needs to be hashed, will complete later!!!
-            new_user = User(username=username,password=password)
+            # hash the users password
+            hashed_password = generate_password_hash(password)
+            # save user credentials to the database
+            new_user = User(username=username,password=hashed_password)
             db.session.add(new_user)
             db.session.commit()
             return render_template('login.html')
 
-
     return render_template('create_account.html',error=error)
 
-@app.route('/authenticate', methods=["POST"])
-def autheticate():
-    form_data = request.form
-    user = form_data["user"]
-    unique = db.session.query(User).filter_by(username=user).first() is None
-    if(unique):
-        u = User(username=user)
-        db.session.add(u)
-        db.session.commit()
-    return redirect(url_for("index", user=user))
-    
     
 # the following is the base for the recipe list screen
 
