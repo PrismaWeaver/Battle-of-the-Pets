28b9179a79a7417a85aad873c5e25effe089c077
Jade Shackelford <j.shackelford@g.austincc.edu>
2023-04-21 15:35:29 -0500

    merged the two search methods into one function, added all other basic methods required for the app, neglecting stretch feature implimentations which have been left commented

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from dotenv import load_dotenv, find_dotenv
 import flask
-from flask import Flask
+import os
+import requests
+
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask, request, render_template, redirect, url_for
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
-import os
 from os import getenv
-import requests
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 # create the app
 app = Flask(__name__)
 
 # load environment variables and set app config settings
 load_dotenv(find_dotenv())
-
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config['SECRET_KEY'] = getenv("SECRET_KEY")
 
@@ -20,9 +22,8 @@ app.config['SECRET_KEY'] = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 
 
-# this will serve as the base for the recipe database
-
-class Recipe_Comments(db.Model): #a model for a row, defines a table
+# These classes form the relations in the database 
+class Comment(db.Model): #a model for a row, defines a table
     id = db.Column(db.Integer, primary_key=True)
     recipe = db.Column(db.Integer, unique=False, nullable=False)
     user = db.Column(db.String(80), unique=False, nullable=False)
@@ -34,12 +35,13 @@ class Saved_Recipes(db.Model):
     user = db.Column(db.String(80), unique=False, nullable=False)
     recipe = db.Column(db.Integer, unique=False, nullable=False)
     
-class User(db.Model):
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(420), nullable=False)
 
-# make sure sql alchemy tables are created
+
+# make sure database tables are created
 with app.app_context():
     db.create_all()
 
@@ -54,93 +56,154 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
-# the following is the base for the login screen
+# Landing Page: If the user is logged in, go directly to the
+# Dashboard, and if not go to the Login Page
+@app.route('/')
+def start():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('login'))
 
-@app.route('/') #this initializes the movie
-def index():
-    #if user is not logged in:
-    return flask.render_template("login.html")
-    #else, display the following:
-    #this page should display the user dashboard
-    #a searchbar at the top which redirects to the search page
-        #this should call the search() function directly below
-    #a display of saved recipes list
-        #query = db.session.query(Saved_Recipes).filter_by(user=current_user).all()
-    #maybe previous comments from the user
-        #query = db.session.query(Recipe_Comments).filter_by(user=current_user).all()
-    #the saved recipe list should have buttons next to each recipe that allows the user
-    #to add said recipe to a shopping list (could be part of the DB, but should be deleted after use), array could work better
 
-@app.route('/login', methods=["POST"])
+# Login Page: from here they can login and be redirected to the Dashboard,
+# or they can go to the create_account page.
+@app.route('/login', methods=['GET','POST'])
 def login():
-    form_data = flask.request.form
-    user = form_data["user"]
-    unique = db.session.query(User).filter_by(username=user).first() is None
-    if(unique):
-        u = User(username=user)
-        db.session.add(u)
+    error = None
+    # if they submitted the login form, check the username and password
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # get a user from the database
+        user = User.query.filter_by(username=username).first()
+
+        # if the user exists and the password matches, log them in
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('start'))
+        # otherwise log an error message
+        else:
+            error = 'Invalid credentials. Please try again.'
+        
+    return render_template('login.html', error=error)
+
+
+# Create Account: you can arrive here from the login page
+# on this page you create a new acocunt to be stored in the database
+@app.route('/create_account',methods=['GET','POST'])
+def create_account():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # get user from the database
+        user = User.query.filter_by(username=username).first()
+
+        # if you were able to find a user, then log error
+        if user:
+            error = 'Account already exists. Try again.'
+        # otherwise go back to the login page
+        else:
+            # hash the users password
+            hashed_password = generate_password_hash(password)
+            # save user credentials to the database
+            new_user = User(username=username,password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            return render_template('login.html')
+
+    return render_template('create_account.html',error=error)
+
+    
+#this displays the dashboard, should always be accessible from every page
+@app.route('/dash')
+@login_required
+def index():
+    saved_list = db.session.query(Saved_Recipes).filter_by(user=current_user.username).all()
+    comments = db.session.query(Comment).filter_by(user=current_user.username).all()
+    #stretch feature for displaying most popular recipes
+    #query the comments per recipe for most popular recipes and display information of the one with the best overall rating
+    #should give higher weight to recipes with more comments?
+    return render_template('dash.html', saved_list = saved_list, comments=comments)
+
+
+#search bar function, should always be accessible from every page
+#POST called if a new search is entered, thus it displays the first page of the new search
+#GET called if next/prev page called of current search
+@app.route('/search/<term>/<indi>', methods=['GET', 'POST']) #id is for the index of the search
+@login_required
+def search(term, indi):
+    if (request.method == 'POST'):
+        load_dotenv(find_dotenv())
+        SPOON_API_SEARCH_URL = "https://api.spoonacular.com/recipes/complexSearch?"
+        spoon_api_response = requests.get(
+            SPOON_API_SEARCH_URL,
+            params={
+                "apiKey": getenv("RECIPE_API"),
+                "query": term
+            },
+        )
+        results = spoon_api_response.json()
+        return render_template('search.html', results=results, term=term, indi=0)
+    else:
+        #indi can be manipulated from the html page, no code needed for separate buttons
+        return render_template('search.html', results=request.args['result'], term=term, indi=indi)
+
+
+#when link from search/saved-list clicked, redirects to this. Uses ID of recipe
+#ID can be accessed via recipe["id"]
+@app.route('/recipe/<recipe_id>')
+@login_required
+def recipe(recipe_id):
+    SPOON_API_GET_RECIPE_URL = "https://api.spoonacular.com/recipes/"
+    spoon_api_response = requests.get(
+            SPOON_API_GET_RECIPE_URL + recipe_id + '/information',
+            params={
+                "apiKey": getenv("RECIPE_API"),
+            },
+        )
+    recipe = spoon_api_response.json()
+    return render_template('recipe.html', recipe=recipe)
+
+
+#this handles both adding comments/ratings & adding to saved recipes
+@app.route('/recipe/<recipe_id>/submit', methods=["POST"])
+@login_required
+def submit(recipe_id):
+    form_data = request.form
+    if(form_data["type"] == 'comment'): #for comment submission
+        c = Comment(recipe = recipe_id, 
+                        user = current_user.username,
+                        rating = form_data["rating"],
+                        comment = form_data["comment"])  
+        db.session.add(c)
         db.session.commit()
-    return flask.redirect(flask.url_for("index", user=user))
-    
-@app.route('/submit', methods=['POST'])
-def search():
-    load_dotenv(find_dotenv())
-    form_data = flask.request.form
-    SPOON_API_SEARCH_URL = "https://api.spoonacular.com/recipes/complexSearch?"
-    spoon_api_response = requests.get(
-        SPOON_API_SEARCH_URL,
-        params={
-            "apiKey": getenv("RECIPE_API"),
-            "query": form_data["query"]
-        },
-    )
-    results = spoon_api_response.json()
-    return flask.redirect(flask.url_for("search_results", results=results, indi=0))
+    else: #for adding to saved list
+        s = Saved_Recipes(recipe = recipe_id, 
+                        user = current_user.username)  
+        db.session.add(s)
+        db.session.commit()
+    return redirect(flask.url_for("recipe"), recipe=recipe_id)
 
-@app.route('/search/<indi>') #indi is for the index of the search
-def search_results():
-    #search should be passed via redirect
-    #this is to keep the same search while navigating pages
-    results = flask.request.args['results']
-    indi = flask.request.args['indi']
-    if(indi < 0): indi = 0
-    
-    #some code to break the search into blocks of 10? 20?
-    #indi refers to the num of blocks to incriment forward by when loading the page
-    
-    return flask.render_template("search.html", results=results, indi=indi)
-    #should a user click on the name of a recipe, its should redirect them to:
-        # url/<recipe id>
-    #these should be called by nav buttons on the search page:
-    #for prev page
-        #return flask.redirect(flask.url_for("search_results", results=results, indi=indi-1))
-    #for next page
-        #return flask.redirect(flask.url_for("search_results", results=results, indi=indi+1))
 
-@app.route('/<recipe>')
-def recipe():
-    recipe = flask.request.args['recipe']
-    #if the recipe info isnt in the redirect, you can fetch it like this
-    SPOON_API_GET_INFO = 'https://api.spoonacular.com/recipes/'
-    spoon_api_response = requests.get(
-        SPOON_API_GET_INFO + recipe + '/information',
-        params={
-            "apiKey": getenv("RECIPE_API"),
-        },
-    )
-    recipe = spoon_api_response
-    return flask.render_template("recipe.html", recipe=recipe)
-
-@app.route('/<recipe>/submit', methods=["POST"])
-def submit():
-    recipe = flask.request.args['recipe']
-    form_data = flask.request.form
-    c = Recipe_Comments(recipe['id'], 
-                      user = form_data["user"],
-                      rating = form_data["rating"],
-                      comment = form_data["comment"])  
-    db.session.add(c)
-    db.session.commit()
-    return flask.redirect(flask.url_for("recipe", recipe=recipe))
+#this will generate the printable shopping list from entries in the saved list
+#currently this will use every recipe in the saved list
+#for stretch features shopping list should be a separate DB that has:
+# >desired servings adjustments, which can be tuned before the final product is produced
+# >ingredient aggrigation, happens once the list is generated
+@app.route('/shopping-list')
+@login_required
+def list_generator():
+    pass
+    #saved_list = db.session.query(Saved_Recipes).filter_by(user=current_user.username).all()
+    #for recipe in saved_list:
+        #use API to collect ingredient information from each saved recipe
+        #create an dictionary with keys for each ingredient
+        # >if key does not exist, set initial value to 0 and add ingredients
+        # >else incriment ingredients by whats required for the recipe 
+    
 
 #app.run()
